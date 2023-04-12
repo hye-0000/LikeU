d7700d4ddf70ad3467e58070853d43764e393bd4
hye-0000 <hye_0000@naver.com>
2023-04-12 14:23:08 +0900

    refactor(불필요한 출력 삭제):

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -42,14 +42,11 @@ public class LikeablePersonController {
     public String add(@Valid AddForm addForm) {
         Member member = rq.getMember();
 
-
         RsData countLikeRsData = likeablePersonService.countLike(member.getInstaMember().getFromLikeablePeople());    //호감 개수 확인
 
         if (countLikeRsData.isFail()) return rq.historyBack(countLikeRsData);
 
         RsData<LikeablePerson> createRsData = likeablePersonService.like(member, addForm.getUsername(), addForm.getAttractiveTypeCode());
-
-
 
         if (createRsData.isFail()) {
             return rq.historyBack(createRsData);
